4f9c3d78f3d2f2cb3835bd71457b98e4e62f95a8
amilasuranjith-dev <amilasuranjith.dev@gmail.com>
2026-02-19 13:27:21 +0530

    feat: Add routing and components for login, dashboard, and customer management
    - Implemented routing for login and dashboard with child routes for customer, item, and order management.
    - Created a customer management interface with a form for adding customers and a table for displaying customer data.
    - Enhanced dashboard with a sidebar navigation and overview components for displaying statistics and recent orders.
    - Developed a login page with form validation and styling.
    - Added CSS styles for improved UI/UX across various components.
    - Introduced a model for customer data structure.

--- src/app/page/dashboard/customer/customer.ts
+++ src/app/page/dashboard/customer/customer.ts
@@ -1,11 +1,41 @@
-import { Component } from '@angular/core';
+import { NgForOf } from '@angular/common';
+import { HttpClient } from '@angular/common/http';
+import { ChangeDetectorRef, Component, OnInit } from '@angular/core';
+import { CustomerModel } from '../../model/type';
+import { FormsModule } from '@angular/forms';
+
 
 @Component({
   selector: 'app-customer',
-  imports: [],
+  imports: [NgForOf, FormsModule],
   templateUrl: './customer.html',
   styleUrl: './customer.css',
 })
-export class Customer {
 
+export class Customer implements OnInit{
+cancel() {
+throw new Error('Method not implemented.');
+}
+save() {
+throw new Error('Method not implemented.');
+}
+  
+  customerList : Array<CustomerModel> = []; 
+
+   // http://localhost:8080/customer/add
+  constructor(private http:HttpClient, private cdr: ChangeDetectorRef){
+    // this.getAll();
+  }
+  ngOnInit(): void {
+    this.getAll();
+  }
+
+  getAll(){
+    this.http.get<CustomerModel[]>("http://localhost:8080/customer/getAll").subscribe(data=>{
+      console.log(data);
+      this.customerList=data;
+      this.cdr.detectChanges();
+    })
+  }
+  
 }
---
+++ src/app/page/model/type.ts
@@ -0,0 +1,15 @@
+export interface CustomerModel{
+    id: string;
+    title: string;
+    name: string;
+    dob: string;
+    salary: number;
+    address: string;
+    city: string;
+    province: string;
+    postalCode: string;
+}
+
+export interface ItemModel{
+
+}
