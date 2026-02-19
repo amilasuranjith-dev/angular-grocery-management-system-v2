e67e07d49ee91887e1fe569c18de1901fcafdc41
amilasuranjith-dev <amilasuranjith.dev@gmail.com>
2026-02-19 14:29:01 +0530

    feat: Integrate SweetAlert2 for customer addition confirmation and update customer form bindings

--- src/app/page/dashboard/customer/customer.ts
+++ src/app/page/dashboard/customer/customer.ts
@@ -3,6 +3,7 @@ import { HttpClient } from '@angular/common/http';
 import { ChangeDetectorRef, Component, OnInit } from '@angular/core';
 import { CustomerModel } from '../../model/type';
 import { FormsModule } from '@angular/forms';
+import Swal from 'sweetalert2';
 
 
 @Component({
@@ -12,30 +13,54 @@ import { FormsModule } from '@angular/forms';
   styleUrl: './customer.css',
 })
 
-export class Customer implements OnInit{
-cancel() {
-throw new Error('Method not implemented.');
-}
-save() {
-throw new Error('Method not implemented.');
-}
-  
-  customerList : Array<CustomerModel> = []; 
 
-   // http://localhost:8080/customer/add
-  constructor(private http:HttpClient, private cdr: ChangeDetectorRef){
-    // this.getAll();
+//--Customer component 
+export class Customer implements OnInit {
+
+  customerList: Array<CustomerModel> = [];
+  customerObj: CustomerModel = {
+    id: '',
+    title: '',
+    name: '',
+    dob: '',
+    salary: 0.0,
+    address: '',
+    city: '',
+    province: '',
+    postalCode: ''
+  }
+
+  // http://localhost:8080/customer/add
+  constructor(private http: HttpClient, private cdr: ChangeDetectorRef) {
   }
   ngOnInit(): void {
     this.getAll();
   }
-
-  getAll(){
-    this.http.get<CustomerModel[]>("http://localhost:8080/customer/getAll").subscribe(data=>{
+  //network respons mapping to CustomerModel
+  getAll() {
+    this.http.get<CustomerModel[]>("http://localhost:8080/customer/getAll").subscribe(data => {
       console.log(data);
-      this.customerList=data;
+      this.customerList = data;
       this.cdr.detectChanges();
     })
   }
-  
+
+  cancel() {
+    throw new Error('Method not implemented.');
+  }
+  addCustomer(): void {
+    // console.log(this.customerObj);
+    this.http.post("http://localhost:8080/customer/add", this.customerObj).subscribe(data => {
+      console.log(data);
+      if (data === true) {
+        Swal.fire({
+          title: "Good job!"+this.customerObj.name+" Saved",
+          text: "You clicked the button!",
+          icon: "success"
+        });
+      }
+      this.getAll();
+    })
+  }
+
 }
